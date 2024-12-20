4ea8a315972120b78b92dd06a690b7e5e89fab60
Julian Woo <julian.woo@student.nhlstenden.com>
2024-12-20 11:43:47 +0100

    added a login

---
+++ pages/authenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $stored_username = "Julianwoo";
+    $stored_password = "Wachtwoord123";
+        //rkPjNnb7uLMuRkskvwKF
+
+
+    if(empty($username) || empty($password)){
+        echo "please enter your username.d";
+        exit();
+    }
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+    if($username === $stored_username && $password === $stored_password){
+        header('Location: portfolio.html');
+    } else {
+        echo "invalid username or password";
+        exit();
+    }
+}
+
+
+?>
---
+++ pages/myscripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebar = document.getElementById("mySidebar");
+    const mainContent = document.getElementById("main");
+    const openBtn = document.querySelector(".openbtn");
+
+    sidebar.style.width = "200px";
+
+
+    openBtn.addEventListener("click", () => {
+        const isOpen = sidebar.style.width === "200px";
+        sidebar.style.width = isOpen ? "0" : "200px";
+    });
+});
+
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
